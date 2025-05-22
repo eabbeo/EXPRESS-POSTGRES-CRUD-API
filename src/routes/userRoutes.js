8d2bf27e9c019661fb7c4e6dd830d2d2a873af20
Edward Abbeo <abbeoedwardiisekou@gmail.com>
2025-05-22 10:00:15 +0000

    completed node with express crud api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,13 +5,14 @@ import { createUser,
      getUserById, 
      updateUser
      } from "../controllers/userController.js";
+import validateUser from "../middleware/inputValidator.js";
 
 const router = express.Router();
 
-router.post("/createUser", createUser);
+router.post("/createUser",validateUser, createUser);
 router.get("/user", getAllUsers);
 router.get("/user/:id", getUserById);
-router.put("/user/:id", updateUser);
+router.put("/user/:id",validateUser, updateUser);
 router.delete("/user/:id", deleteUser);
 
 export default router;
